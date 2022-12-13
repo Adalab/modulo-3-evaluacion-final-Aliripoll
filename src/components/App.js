7f14e57cc254c9dbcd3b06e1b34a05705a884d14
Aliripoll <aripollortega@gmail.com>
2022-12-13 12:10:10 +0100

    update fetch callToApi and Clean Data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useEffect } from "react";
 import CharacterList from "./CharacterList";
 import FilterByName from "./FilterByName";
 import FilterBySpecies from "./FilterBySpecies";
+//services
+import callToApi from "../services/api";
 //routes
 import { Routes, Route } from "react-router-dom";
 //styles
@@ -10,11 +13,11 @@ import "../styles/App.scss";
 function App() {
   //STATE VARIABLES
   //USE EFFECT
-  /*useEffect (()=> {
-    callToApi().then ((data) => {
+  useEffect(() => {
+    callToApi().then((data) => {
       console.log(data);
     });
-  }, []);*/
+  }, []);
   //EVENT FUNCTIONS
   //RENDER FUNCTIONS
   //RETURN
--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,17 @@
 const callToApi = () => {
-  return fetch('https://rickandmortyapi.com/api/character');
-        .then ((response) => response.json())
-        .then ((data) => {
-            const cleanData = data.map((eachData) => {
-                return {
-                    id: eachData.id,
-                    image: eachData.imageUrl,
-                    name: eachData.name,
-                    species: eachData.species
-                }
- 
-  })
-  return cleanData;
-})
+  return fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((data) => {
+      const cleanData = data.results.map((character) => {
+        return {
+          id: character.id,
+          image: character.image,
+          name: character.name,
+          species: character.species,
+        };
+      });
+      return cleanData;
+    });
 };
 
 export default callToApi;
